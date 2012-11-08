ecabb31c8283daf70d56fb7504533aa38eb831c2
Chengwu <chengwhuang@gmail.com>
2012-11-08 17:37:41 +0100

    Ajout d'une méthode set_size() qui renvoie la taille du sprite

--- src/enemy.hpp
+++ src/enemy.hpp
@@ -10,6 +10,10 @@ class Enemy: public VisibleObject
             
         virtual void update(const sf::RenderWindow& fen);
         
+        void set_position(float x, float y) { _sprite.SetPosition(x, y); }
+        const sf::Vector2f& get_position() const { return _sprite.GetPosition(); }
+        sf::Vector2f get_size() const { return _sprite.GetSize(); }
+        
     protected:
         // Constructeur protégé pour préciser que c'est une classe abstraite
         Enemy(const std::string& filename, int z=1) : VisibleObject(filename, z) { }
