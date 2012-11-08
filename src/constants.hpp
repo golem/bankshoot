e1a8c0d07572c90b05f468a310d9eb61f23d1ebf
Chengwu <chengwhuang@gmail.com>
2012-11-08 17:44:48 +0100

    Quelques constantes

---
+++ src/constants.hpp
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+/**
+ * @file constant.hpp
+ * @brief Définition des constantes
+ */
+
+const int SCREEN_WIDTH = 800; /**< Largeur de la fenêtre */
+const int SCREEN_HEIGHT = 600; /**< Hauteur de la fenêtre */
+
+const int PROPA_MAFIA = 2;
+const float NEXT_ENEMY_TIME = 2.0f;
+
+#endif
