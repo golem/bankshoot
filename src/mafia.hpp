77de0d232fb9ed534665bfc4fd2a2f3ee61449fc
Chengwu <chengwhuang@gmail.com>
2012-11-08 17:45:46 +0100

    Un autre type d'ennemi, juste pour le test

---
+++ src/mafia.hpp
@@ -0,0 +1,20 @@
+#ifndef MAFIA_HPP
+#define MAFIA_HPP
+
+#include "enemy.hpp"
+
+class Mafia: public Enemy
+{
+    public:
+        Mafia (const std::string& filename) : Enemy(filename)
+        {
+            // TODO : mieux gérer ces paramètres, peut être que ça devrait pas être
+            //défini dans la superclasse, ou alors fournir un constructeur correct
+            _vx = 0.0f;
+            _vy = 200.0f;
+        }
+        
+        void update(const sf::RenderWindow& fen);
+};
+
+#endif
