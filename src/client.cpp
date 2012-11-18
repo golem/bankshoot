0b0fd1dd7a3f2cf3a01cd2ac6269b5ce1d7fd666
Chengwu <chengwhuang@gmail.com>
2012-11-18 17:18:47 +0100

    Petite animation pour le client

--- src/client.cpp
+++ src/client.cpp
@@ -5,11 +5,19 @@ void* Client::update(const sf::RenderWindow& fen)
     float dt = fen.GetFrameTime();
     sf::Vector2f pos = _sprite.GetPosition();
     sf::Vector2f size = _sprite.GetSize();
-    
+
     pos.y += _vy*dt;
     pos.x += _vx*dt*sin(pos.y*3.14159f/180.0f);
     _sprite.SetPosition(pos);
-    
+
+    // petite animation, pour voir de la fumée qui sort par les oreilles
+    if ((_elapsed_time += dt) > 0.2f) {
+        _mood = !_mood;
+        if (_mood == true) _sprite.SetSubRect(_masque_normal);
+        else _sprite.SetSubRect(_masque_angry);
+        _elapsed_time = 0.0f;
+    }
+
     // Contrairement aux autres ennemis, on ne meurt pas en sortant
     //sur les côtés de l'écran, ça serait un peu dommage je pense.
     if ((pos.y <  -size.y) || (pos.y > fen.GetHeight())) {
--- src/client.hpp
+++ src/client.hpp
@@ -19,10 +19,15 @@ class Client : public Enemy
          *
          * @param filename Le chemin de l'image à afficher.
          **/
-        Client(const std::string& filename) : Enemy(filename)
+        Client(const std::string& filename) : Enemy(filename),
+                                              _masque_normal(0, 0, 31, 31),
+                                              _masque_angry(32, 0, 81, 31),
+                                              _elapsed_time(0.0f),
+                                              _mood(false)
         {
             _vx = CLIENT_AMPLITUDE;
             _vy = CLIENT_SPEED;
+           _sprite.SetSubRect(_masque_normal);
         }
         
         /**
@@ -34,6 +39,12 @@ class Client : public Enemy
         * @return void*
         **/
         void * update(const sf::RenderWindow& fen);
+
+    private:
+        const sf::IntRect _masque_normal; ///< Masque pour extraire le bon sprite de la sprite sheet.
+        const sf::IntRect _masque_angry; ///< Masque pour extraire le bon sprite de la sprite sheet.
+        float _elapsed_time;
+        bool _mood;
 };
 
 #endif // CLIENT_HPP
