2c15dcc60959458f5c8faac268d65b119e36ec2f
Chengwu <chengwhuang@gmail.com>
2012-11-20 18:42:07 +0100

    Ajout d'un attribut '_id' pour connaitre le tireur

--- src/projectile.hpp
+++ src/projectile.hpp
@@ -18,14 +18,28 @@
 class Projectile: public CollidingObject
 {
     public:
+    
+        /**
+         * @enum shot_id
+         * @brief Connaître l'auteur des tirs
+         */
+        enum shot_id {
+            banker, ///< Projectiles appartenant au banquier
+            enemy   ///< Projectiles appartenant aux ennemis
+        };
 
         /**
          * @brief Constructeur
-         * 
+         * @param filename
+         * @param x
+         * @param y
+         * @param id
+         * @param vy
+         * @param vx
          * Appelle le constructeur de \a CollidingObject
          */
-        Projectile(const std::string& filename, float x, float y, float vy=-BANKER_SHOT_SPEED):
-            CollidingObject(filename, 20), _vy(vy)
+        Projectile(const std::string& filename, float x, float y, shot_id id, float vy=-BANKER_SHOT_SPEED, float vx=0.0f):
+            CollidingObject(filename, 20), _id(id), _vy(vy), _vx(vx)
         {
             // TODO: Réfléchir à améliorer ce constructeur (vitesse +/-, position par rapport au créateur, etc.)
            _sprite.SetPosition(x, y);
@@ -38,13 +52,18 @@ class Projectile: public CollidingObject
          
          /**
           * @brief Mettre à jour la position
+          * @param fen
           */
         void * update(const sf::RenderWindow& fen);
         
         virtual void * collision(CollidingObject * o);
         
+        
+        shot_id get_shot_id() { return _id; }
     private:
-        float _vy; /**< Vitesse suivant l'ordonné */
+        shot_id _id;  ///< Identifiant du tireur
+        float _vy;  ///< Vitesse suivant l'ordonné
+        float _vx;  ///< Vitesse suivant l'abscisse
 };
 
 #endif
