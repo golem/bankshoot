78dfb04cddb616c60cca85129c825fbf0dbd0e6e
Chengwu <chengwhuang@gmail.com>
2012-11-20 18:47:09 +0100

    Affiche le point de vie du banquier

--- src/scoredisplay.cpp
+++ src/scoredisplay.cpp
@@ -10,7 +10,8 @@ void* ScoreDisplay::update(const sf::RenderWindow& fen)
     fen.GetFrameTime();
     if (_banquier != NULL) {
         std::ostringstream stream;
-        stream << "Score : " << _banquier->get_score();
+        stream << "Score : " << _banquier->get_score() << std::endl;
+        stream << "Vie :" << _banquier->get_life();
         _text.SetText(stream.str());
     }
     return NULL;
